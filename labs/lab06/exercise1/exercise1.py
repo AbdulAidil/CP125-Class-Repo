47902de50eb4d1344cba5575be6c35d2942c682a
AbdulAidil <abdulaidilshahhh@gmail.com>
2026-01-26 13:02:54 +0800

    26 / 01 /2026

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,2 +1,7 @@
 def get_legit_power_users(log_data, bot_ids, threshold):
-    pass
+    power_users = [] 
+    valid_users = set()
+
+    for time, user_id, action in log_data:
+        if user_id not in bot_ids:
+            valid_users.add(user_id)
--- labs/lab06/exercise2/exercise2.py
+++ labs/lab06/exercise2/exercise2.py
@@ -1,2 +1,20 @@
 def match_specialists(candidates_list, project_requirements):
-    pass
+    skill_count = {}
+    for name, skills in candidates_list:
+        for skill in skills:
+            if skill not in skill_count:
+                skill_count[skill] = 0
+            else:
+                skill_count[skill] += 1
+
+    rare_skill = set()
+    skill, count = skill_count
+    for skill in skill_count:
+        if count < 3:
+            rare_skill.add(skill)
+    
+    specialist = []
+    for name, skills in candidates_list:
+        missing_requirement = project_requirements - skills
+        if missing_requirement == set():
+            my
